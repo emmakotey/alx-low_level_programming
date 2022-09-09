d0a19fc3ecc2d7b438acea74ad6bcf22d9fa9557
emmakotey <emmakotey@gmail.com>
2022-09-08 17:18:50 -0700

    add

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *  * main - Entry point
- *
+ *  *main-Serves as the entry point of a program
  *   *
- *
- *    * Return: Always 0 (Success)
- *
+ *    *Return:if no error return zero, otherwise return non zero value
  *     */
-
+ 
 int main(void)
-
 {
-
-		printf("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+    printf("with proper grammar, but the outcome is a piece of art,\n.");
+    return (0);
 }
