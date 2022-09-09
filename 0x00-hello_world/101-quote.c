22aaee150255794b7655b15f217a0dce9b9d314a
emmakotey <emmakotey@gmail.com>
2022-09-08 20:29:50 -0700

    test

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,18 @@
-nclude <stdio.h>
-
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- *
- * * main - print quote
- *
- * * Description: prints Dora's quote
- *
- * * Return: Always 1 (Success)
- *
- * */
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
 
 int main(void)
 
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	write(STDOUT_FILENO, "and that piece of art is useful
+
+			\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 
