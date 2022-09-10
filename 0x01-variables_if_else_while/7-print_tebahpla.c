47787d7da6e4c8764fa25afe1190d42fc2c5eea5
PSL-CUDJOE <PSL-CUDJOE@PSL-CUDJOE.persolgh.com>
2022-09-10 07:14:41 +0000

    done1

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,4 +1,4 @@
-#iinclude <stdio.h>
+#include <stdio.h>
 
 /**
  * main - Prints reversed alphabet.
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-int i;
-for (i = 122; i > 96; i--)
-{
-putchar(i);
-}
-putchar('\n');
-return (0);
+	int i;
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 } 
