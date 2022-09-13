a848cc5d124a880653f4868bbd7259b2010d9648
emmakotey <emmakotey@gmail.com>
2022-09-13 13:05:51 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,43 +1,21 @@
 #include "main.h"
 
 
-
 /**
-
- * main - prints the word _putchar
-
- *
-
- * Return: 0 on success execution.
-
+ * main - prints putchar
+ * Return: always 0
  */
 
-
-
 int main(void)
-
 {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
-        char s[] = "_putchar";
-
-        int i = 0;
-
-
-
-        while (s[i] != '\0')
-
-        {
-
-                _putchar(s[i]);
-
-                i++;
-
-        }
-
-        _putchar(10);
-
-
-
-        return (0);
-
+	return (0);
 }
