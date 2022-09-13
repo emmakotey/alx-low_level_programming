e592c418cc0b36518fa5b327fa5dffe5e01a8c88
emmakotey <emmakotey@gmail.com>
2022-09-13 16:05:11 -0700

    test

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,25 @@
-#include "main.h"
+include "main.h"
 
 
 /**
- * main - prints putchar
- * Return: always 0
+ * main - prints the word _putchar
+ *
+ * Return: 0 on success execution.
  */
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+        char s[] = "_putchar";
+        int i = 0;
+        while (s[i] != '\0')
+        {
+                _putchar(s[i]);
 
-	return (0);
+                i++;
+        }
+        _putchar(10);
+
+        return (0);
 }
+
+
