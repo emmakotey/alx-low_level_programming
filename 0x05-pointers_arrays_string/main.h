0de8a00d1945e46e04abe589d34513b2226991eb
emmakotey <emmakotey@gmail.com>
2022-09-19 04:18:54 -0700

    reset to 98

--- 0x05-pointers_arrays_string/main.h
+++ 0x05-pointers_arrays_string/main.h
@@ -1,7 +1,10 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
-int _putchar(char c);
+/**
+ * void prototypes()
+ * int _putchar(char c);
+ */
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
