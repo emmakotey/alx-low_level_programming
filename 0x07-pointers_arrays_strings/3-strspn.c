517a0639b2dd5d1574948363da23aac4ac86eef6
emmakotey <emmakotey@gmail.com>
2022-09-26 08:58:14 -0700

    up

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,7 +4,7 @@
  * *_strspn - gets the length of a prefix substring
  *
  * @s: string to evaluate
- * @acccept: string containing the list of characters to match in s
+ * @accept: string containing the list of characters to match in s
  *
  * Return: the number of bytes in the initial segment
  * of s which consist only of bytes from accept
