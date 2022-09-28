9b55ff5ebad6773e84d38fb701bdeeee51d6b78c
emmakotey <emmakotey@gmail.com>
2022-09-28 01:52:27 -0700

    test

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * is_prime - prime
+ * @x: number
+ * @y: number
+ * Return: result 0 not prime 1 prime
+ * is_prime_number - verifies that the number is prime
+ * @n: verify number
+ * Return: result 0 not prime 1 prime
+ */
+int is_prime(int x, int y)
+{
+	if ((x % y) == 0 && y < x)
+		return (0);
+	if (x == y)
+		return (1);
+	else
+		return (is_prime(x, y + 1));
+}
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (is_prime(n, 2));
+}
