36de228a395a00ba14d8f330e9b9f74d837256ea
emmakotey <emmakotey@gmail.com>
2022-10-06 12:35:04 -0700

    message

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -29,4 +29,25 @@ int i, j;
 	nul = atol(argv[1]) * atol(argv[2]);
 	printf("%lu\n", nul);
 return (0);
+
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
+
+/**
+ * find_len - Finds the length of a string.
+ * @str: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
+int find_len(char *str)
+{
+	int len = 0;
+
+	while (*str++)
+		len++;
+
+	return (len);
 }
