6b2a46acc655432f728a8dee8081f48d336be698
emmakotey <emmakotey@gmail.com>
2022-12-13 22:34:49 +0000

    no

--- 0x18-dynamic_libraries/3-puts.c
+++ 0x18-dynamic_libraries/3-puts.c
@@ -1,23 +1,18 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * _puts - function that prints a string foolowed by new line to stdout
- * @str: string to print
- * Return: void
+ * _puts - prints a string
+ *
+ * @str: string to be printed
  */
 
 void _puts(char *str)
 {
 	int i;
-	char c;
 
-	c = *str;
-	i = 0;
-	while (c)
+	for (i = 0; str[i]; i++)
 	{
-		_putchar(c);
-		i++;
-		c = *(str + i);
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
